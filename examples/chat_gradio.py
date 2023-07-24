7b0ac5d7a2c280d1a094888a5980d852710fe7c7
torahiko <92130911+stormdragongardin@users.noreply.github.com>
2023-07-24 11:09:31 +0800

    Update chat_gradio.py

--- examples/chat_gradio.py
+++ examples/chat_gradio.py
@@ -91,4 +91,4 @@ if __name__ == "__main__":
     streamer = TextIteratorStreamer(tokenizer,skip_prompt=True)
     if torch.__version__ >= "2" and sys.platform != "win32":
         model = torch.compile(model)
-    demo.queue().launch(share=False,debug = True)
+    demo.queue().launch(share=False,debug = True,port='8000')
