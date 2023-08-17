6232280d043df943d6c2ad4ec421e71d5a06856c
zhangzheng <zhangzheng@atomecho.xyz>
2023-08-17 16:21:40 +0800

    update example

--- examples/chat_gradio.py
+++ examples/chat_gradio.py
@@ -31,7 +31,7 @@ with gr.Blocks() as demo:
         prompt = ''
         if slider_context_times>0:
             prompt += '\n'.join([("<s>Human: "+one_chat[0].replace('<br>','\n')+'\n</s>' if one_chat[0] else '')  +"<s>Assistant: "+one_chat[1].replace('<br>','\n')+'\n</s>'    for one_chat in history_true[-slider_context_times:] ])
-        prompt +=  "<s>Human: "+history[-1][0].replace('<br>','\n')+"\n</s><s>Assistant: "
+        prompt +=  "<s>Human: "+history[-1][0].replace('<br>','\n')+"\n</s><s>Assistant:"
         input_ids = tokenizer([prompt], return_tensors="pt",add_special_tokens=False).input_ids[:,-512:].to('cuda')        
         generate_input = {
             "input_ids":input_ids,
