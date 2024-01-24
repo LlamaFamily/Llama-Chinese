a5ac81f64d84030c86ab9af75fe89b7a0b55d0f2
wuyongyu <wuyongyu@atomecho.xyz>
2024-01-24 13:19:05 +0800

    add trust_remote_code for vllm

--- inference-speed/GPU/vllm_example/api_server.py
+++ inference-speed/GPU/vllm_example/api_server.py
@@ -71,6 +71,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--host", type=str, default="0.0.0.0")
     parser.add_argument("--port", type=int, default=8090)
+    parser.add_argument("--trust_remote_code", type=bool, default=True)
     parser = AsyncEngineArgs.add_cli_args(parser)
     args = parser.parse_args()
 
